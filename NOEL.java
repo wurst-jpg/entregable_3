19ac3f5a149666df0f65416b1f4a2603f69075e1
wurst-jpg <wurs784@gmail.com>
2026-09-07 23:11:23 -0500

    creando el array y la interfaz que va a aparecer en la terminal

---
+++ NOEL.java
@@ -0,0 +1,31 @@
+public class NOEL {
+    
+    public static void main(String[] args) {
+        String[] productos = new String[5];
+        productos[0] = "Ducales";
+        productos[1] = "Festival";
+        productos[2] = "Tosh";
+        productos[3] = "Null";
+        productos[4] = "Null";
+        boolean salir = false;
+        do {
+            System.out.println("╔══════════════════════════════════════════╗\n" + //
+                                "║    ███╗   ██╗ ██████╗ ███████╗ ██       ║\n" + //
+                                "║    ████╗  ██║██╔═══██╗██╔════╝ ██       ║\n" + //
+                                "║    ██╔██╗ ██║██║   ██║█████╗   ██       ║\n" + //
+                                "║    ██║╚██╗██║██║   ██║██╔══╝   ██       ║\n" + //
+                                "║    ██║ ╚████║╚██████╔╝███████╗ ███████╗ ║\n" + //
+                                "║    ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝ ╚══════╝ ║\n" + //
+                                "║   ────────────────────────────────────   ║\n" + //
+                                "║   1. Listar Productos                    ║\n" + //
+                                "║   2. Buscar Producto                     ║\n" + //
+                                "║   3. Agregar Producto                    ║\n" + //
+                                "║   4. Salir                               ║\n" + //
+                                "║   ────────────────────────────────────   ║\n" + //
+                                "║   Seleccione una opción: █               ║\n" + //
+                                "╚══════════════════════════════════════════╝");
+        } while (salir = false);
+
+    }
+    
+}
